ac5f543a5097a46fc33d96feac9bcdac4ec9011a
limpbot <leonhardsommer96@gmail.com>
2023-06-15 08:37:14 +0200

    add random solarize

--- src/od3d/cv/transforms/rgb.py
+++ src/od3d/cv/transforms/rgb.py
@@ -20,6 +20,7 @@ class RGB_Random:
         self.transform = torchvision.transforms.Compose([
             #torchvision.transforms.RandomApply(
             #    torchvision.transforms.GaussianBlur(kernel_size=9, sigma=(0.1, 2.0)), p=0.1),
+            torchvision.transforms.RandomSolarize(threshold=128, p=0.2),
             torchvision.transforms.RandomGrayscale(p=0.2),
             torchvision.transforms.RandomApply(
                 [torchvision.transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1)],
