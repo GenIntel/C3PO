89fa4a9ac603232a2329bb877b93c2af42260a8f
limpbot <leonhardsommer96@gmail.com>
2023-07-25 09:23:42 +0200

    speed up crop, by differentiating with scale

--- src/od3d/cv/visual/crop.py
+++ src/od3d/cv/visual/crop.py
@@ -1,3 +1,5 @@
+import logging
+logger = logging.getLogger(__name__)
 import math
 import torch
 from od3d.cv.visual.resize import resize
@@ -14,28 +16,48 @@ def crop(img, center, H_out, W_out, scale=1., ctx=None):
                                 math.floor(center[1] - bbox_in_shape_yhalf),
                                 math.ceil(center[0] + bbox_in_shape_xhalf),
                                 math.ceil(center[1] + bbox_in_shape_yhalf)]).to(device)
-
     # x-, x+, y-, y+
-    pad = [max(-bbox_in[0], 0), max(bbox_in[2] - img_in_shape[1], 0), max(-bbox_in[1], 0),
-           max(bbox_in[3] - img_in_shape[0], 0)]
+    pad_in = torch.LongTensor([max(-bbox_in[0], 0), max(bbox_in[2] - img_in_shape[1], 0), max(-bbox_in[1], 0),
+              max(bbox_in[3] - img_in_shape[0], 0)]).to(device)
 
-    #img = torch.nn.functional.pad(img, pad=pad)
 
-    img_padded = torch.zeros(size=img.shape[:-2] + torch.Size([img.shape[-2]+pad[2]+pad[3], img.shape[-1]+pad[0]+pad[1]]), dtype=img.dtype, device=device)
-    pad_x_upper = -pad[1] if pad[1] > 0 else None
-    pad_y_upper = -pad[3] if pad[3] > 0 else None
-    img_padded[:, pad[2]:pad_y_upper, pad[0]:pad_x_upper] = img
-    img_crop_bbox_in = img_padded[:, bbox_in[1]+pad[2]: bbox_in[3]+pad[2], bbox_in[0]+pad[0]:bbox_in[2]+pad[0]]
 
-    img_out = resize(img_crop_bbox_in, H_out=H_out, W_out=W_out)
+    # two options:
+    # a) first crop then resize (preferred if scale > 1. -> pad on lower-resolution image)
+    if scale >= 1.:
+        # img = torch.nn.functional.pad(img, pad=pad)
+        img_padded = torch.zeros(
+            size=img.shape[:-2] + torch.Size([img.shape[-2] + pad_in[2] + pad_in[3], img.shape[-1] + pad_in[0] + pad_in[1]]),
+            dtype=img.dtype, device=device)
+        pad_x_upper = -pad_in[1] if pad_in[1] > 0 else None
+        pad_y_upper = -pad_in[3] if pad_in[3] > 0 else None
+        img_padded[:, pad_in[2]:pad_y_upper, pad_in[0]:pad_x_upper] = img
+        img_cropped = img_padded[:, bbox_in[1] + pad_in[2]: bbox_in[3] + pad_in[2], bbox_in[0] + pad_in[0]:bbox_in[2] + pad_in[0]]
+        img_out = resize(img_cropped, H_out=H_out, W_out=W_out)
+
+
+    # b) first resize then crop (preferred if scale < 1. -> pad on lower-resolution image)
+    else:
+        img_res = resize(img, scale_factor=scale)
+        bbox_in_res = (bbox_in * scale).to(torch.long)
+        bbox_in_res[2] = W_out + bbox_in_res[0]
+        bbox_in_res[3] = H_out + bbox_in_res[1]
+        pad_in_res = (pad_in * scale).to(torch.long)
+        img_padded = torch.zeros(
+            size=img_res.shape[:-2] + torch.Size(
+                [H_out + pad_in_res[2] + pad_in_res[3], W_out + pad_in_res[0] + pad_in_res[1]]),
+            dtype=img_res.dtype, device=device)
+        img_padded[:, pad_in_res[2]:pad_in_res[2]+img_res.shape[1], pad_in_res[0]:pad_in_res[0]+img_res.shape[2]] = img_res
+        img_out = img_padded[:, bbox_in_res[1] + pad_in_res[2]: bbox_in_res[3] + pad_in_res[2],
+                      bbox_in_res[0] + pad_in_res[0]:bbox_in_res[2] + pad_in_res[0]]
 
     if ctx is not None:
+        bbox_out = torch.LongTensor([math.ceil(pad_in[0] * scale),
+                                     math.ceil(pad_in[2] * scale),
+                                     math.floor(W_out - 1 - pad_in[1] * scale),
+                                     math.floor(H_out - 1 - pad_in[3] * scale)]).to(device)
         ctx = resize(ctx, H_out=H_out, W_out=W_out)
-        bbox_img = torch.LongTensor([math.ceil(pad[0] * scale),
-                                     math.ceil(pad[2] * scale),
-                                     math.floor(W_out - 1 - pad[1] * scale),
-                                     math.floor(H_out - 1 - pad[3] * scale)]).to(device)
-        ctx[:, bbox_img[1]:bbox_img[3], bbox_img[0]:bbox_img[2]] = img_out[:, bbox_img[1]: bbox_img[3], bbox_img[0]:bbox_img[2]]
+        ctx[:, bbox_out[1]:bbox_out[3], bbox_out[0]:bbox_out[2]] = img_out[:, bbox_out[1]: bbox_out[3], bbox_out[0]:bbox_out[2]]
         img_out = ctx
 
     cam_crop_tform_cam = torch.Tensor([[scale, 0., -bbox_in[0] * scale, 0.],
