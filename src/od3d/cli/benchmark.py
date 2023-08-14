6b91e386dda32e6ba25caf7fa58c16167d71d36d
limpbot <leonhardsommer96@gmail.com>
2023-08-14 14:52:55 +0200

    add backbone ablation

--- src/od3d/cli/benchmark.py
+++ src/od3d/cli/benchmark.py
@@ -13,17 +13,24 @@ from omegaconf import open_dict
 import time
 
 import datetime
+import pandas as pd
 
 from tabulate import tabulate
-@app.command()
-def table():
+
+def get_nested_value(data, key):
+    keys = key.split('.')  # Split the string key into a list of keys
+    value = data
+    for k in keys:
+        if k in value:
+            value = value[k]
+        else:
+            return None  # Key not found
+    return value
+
+def get_dataframe(configs=[], metrics=[], name_partial=None, age_in_hours=None):
     logging.basicConfig(level=logging.INFO)
     import wandb
     config = od3d.io.load_hierarchical_config()
-
-    #metrics = ['test/pascal3d_test/pose/acc_pi6', 'test/pascal3d_test/pose/acc_pi18', 'test/pascal3d_test/pose/err_median', 'test/pascal3d_test/pose/err_mean']
-    #metrics = ['test/co3d_5s_test/pose/acc_pi6', 'test/co3d_5s_test/pose/acc_pi18', 'test/co3d_5s_test/pose/err_median', 'test/co3d_5s_test/pose/err_mean']
-    metrics = ['test/co3d_50s_test/pose/acc_pi6', 'test/co3d_50s_test/pose/acc_pi18', 'test/co3d_50s_test/pose/err_median', 'test/co3d_50s_test/pose/err_mean']
 
     # Initialize wandb
      # wandb.init(project=config.logger.wandb_project_name)
@@ -36,11 +43,17 @@ def table():
     # Fetch all the runs in your project
     runs = api.runs(config.logger.wandb_project_name)
 
+    if age_in_hours is not None:
+        runs = list(filter(
+            lambda run: get_timestamp_from_string(run.name) > datetime.datetime.now() - datetime.timedelta(hours=age_in_hours),
+            runs))
 
-    runs = list(filter(lambda run: all([metric in list(run.summary.keys()) for metric in metrics]), runs))
+    if name_partial is not None:
+        runs = list(filter(lambda run: name_partial in run.name, runs))
 
-    runs = list(filter(lambda run: 'multiview' in run.name, runs))
-    runs = list(filter(lambda run: get_timestamp_from_string(run.name) > datetime.datetime.now() - datetime.timedelta(hours=15), runs))
+    if metrics is not None:
+        runs = list(filter(lambda run: all([metric in list(run.summary.keys()) for metric in metrics]), runs))
+
 
     rows = []
     for run in runs:
@@ -48,8 +61,10 @@ def table():
             logger.info(f'runs {run.name}')
             run_summary = run.summary
             row = [run.name]
-            row.append(json.loads(run.json_config)["method"]['value']['multiview']['type'].replace('_', ' '))
-            row.append(json.loads(run.json_config)["method"]['value']['multiview']['batch_size'])
+            if len(configs) > 0:
+                json_config = json.loads(run.json_config)
+            for config in configs:
+                rows.append(get_nested_value(json_config, key=config))
             for metric in metrics:
                 row.append(run_summary[metric])
 
@@ -61,10 +76,45 @@ def table():
 
     #cols = ['name'] + metrics
 
-    cols = ['name', 'type', 'batch_size'] + metrics
+    cols = ['name'] + configs + metrics
 
-    import pandas as pd
-    my_df = pd.DataFrame(rows, columns=cols)
+    df = pd.DataFrame(rows, columns=cols)
+
+    logger.info(tabulate(rows, headers=cols, tablefmt='github')) # 'github', 'tsv'
+
+    return df
+@app.command()
+def table():
+    logging.basicConfig(level=logging.INFO)
+    import wandb
+    config = od3d.io.load_hierarchical_config()
+
+    #metrics = ['test/pascal3d_test/pose/acc_pi6', 'test/pascal3d_test/pose/acc_pi18', 'test/pascal3d_test/pose/err_median', 'test/pascal3d_test/pose/err_mean']
+    metrics = ['test/co3d_5s_test/pose/acc_pi6', 'test/co3d_5s_test/pose/acc_pi18', 'test/co3d_5s_test/pose/err_median', 'test/co3d_5s_test/pose/err_mean']
+    # metrics = ['test/co3d_50s_test/pose/acc_pi6', 'test/co3d_50s_test/pose/acc_pi18', 'test/co3d_50s_test/pose/err_median', 'test/co3d_50s_test/pose/err_mean']
+    name_partial = None
+    # configs = ['method.value.multiview.type', 'method.value.multiview.batch_size']
+    age_in_hours = 300
+    configs = []
+
+    my_df = get_dataframe(configs=configs, metrics=metrics, age_in_hours=age_in_hours, name_partial=name_partial)
+    my_df.to_csv('output.csv', index=False, header=False, float_format='%.3f')
+
+
+@app.command()
+def table_multiview():
+    logging.basicConfig(level=logging.INFO)
+    import wandb
+    config = od3d.io.load_hierarchical_config()
+
+    #metrics = ['test/pascal3d_test/pose/acc_pi6', 'test/pascal3d_test/pose/acc_pi18', 'test/pascal3d_test/pose/err_median', 'test/pascal3d_test/pose/err_mean']
+    #metrics = ['test/co3d_5s_test/pose/acc_pi6', 'test/co3d_5s_test/pose/acc_pi18', 'test/co3d_5s_test/pose/err_median', 'test/co3d_5s_test/pose/err_mean']
+    metrics = ['test/co3d_50s_test/pose/acc_pi6', 'test/co3d_50s_test/pose/acc_pi18', 'test/co3d_50s_test/pose/err_median', 'test/co3d_50s_test/pose/err_mean']
+    name_partial = 'multiview'
+    configs = ['method.value.multiview.type', 'method.value.multiview.batch_size']
+    age_in_hours = 300
+
+    my_df = get_dataframe(configs=configs, metrics=metrics, age_in_hours=age_in_hours, name_partial=name_partial)
 
     import seaborn as sns
     import matplotlib.pyplot as plt
@@ -73,8 +123,8 @@ def table():
     my_df[metrics[0]] *= 100
     my_df[metrics[1]] *= 100
 
-    my_df = my_df.groupby(['type', 'batch_size']).head(1)
-    my_df = my_df.rename(columns={'batch_size': 'multiview #frames', metrics[0]: metrics_new_names[0], metrics[1]: metrics_new_names[1], metrics[2]: metrics_new_names[2], metrics[3]: metrics_new_names[3]})
+    my_df = my_df.groupby(['method.value.multiview.type', 'method.value.multiview.batch_size']).head(1)
+    my_df = my_df.rename(columns={'method.value.multiview.type': 'type', 'method.value.multiview.batch_size': 'multiview #frames', metrics[0]: metrics_new_names[0], metrics[1]: metrics_new_names[1], metrics[2]: metrics_new_names[2], metrics[3]: metrics_new_names[3]})
     my_df = my_df.sort_values(by=['type'])
 
     for i, metric in enumerate(metrics_new_names):
@@ -87,9 +137,9 @@ def table():
         )
         #fig = mv_plot.get_figure()
         plt.savefig(f"multiview_{metrics[i].replace('/', '_')}.png")
-    logger.info(tabulate(rows, headers=cols, tablefmt='github')) # 'github', 'tsv'
 
     my_df.to_csv('output.csv', index=False, header=False)
+
 
 
 @app.command()
--- src/od3d/models/backbones/dino/backbone.py
+++ src/od3d/models/backbones/dino/backbone.py
@@ -3,43 +3,50 @@ logger = logging.getLogger(__name__)
 from torch import nn
 from omegaconf import DictConfig
 import torch
-from od3d.cv.visual.resize import resize
 from od3d.cv.transforms import RGB_UInt8ToFloat, RGB_Normalize # , CenterZoom3D, RGB_Random
 import torchvision
 from od3d.models.backbones.backbone import OD3D_Backbone
+from od3d.data.ext_enum import ExtEnum
+from od3d.cv.visual.resize import resize
+
+class DINOv2_WEIGHTS(str, ExtEnum):
+    DEFAULT = 'default'
+    NONE = 'none'
+
 
 class DINOv2(OD3D_Backbone):
-    def __init__(self, config: DictConfig):
-        super().__init__(config=config)
-        # dinov2_vits14 dinov2_vitb14 dinov2_vitl14 dinov2_vitg14
-        self.net = torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14')
+    def __init__(
+        self,
+        config: DictConfig
+    ):
 
-        for param in self.net.parameters():
-            param.requires_grad = False
+        super().__init__(config=config)
 
         self.transform = torchvision.transforms.Compose([
                 RGB_UInt8ToFloat(),
                 RGB_Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
 
-        self.dino_feat_dim = 384
-        self.feat_dim = config.channels # 128
 
-        self.doubleconv = nn.Sequential(
-            nn.Conv2d(self.dino_feat_dim, self.feat_dim, kernel_size=3, padding=1),
-            nn.BatchNorm2d(self.feat_dim),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(self.feat_dim, self.feat_dim , kernel_size=3, padding=1),
-            nn.BatchNorm2d(self.feat_dim),
-            nn.ReLU(inplace=True),
-        )
+        # dinov2_vits14 dinov2_vitb14 dinov2_vitl14 dinov2_vitg14
+        self.extractor = torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14', pretrained=self.config.weights=='default')
 
-    # 'x_norm_patchtokens', 'x_prenorm'
+        self.layers_returned = config.layers_returned # choose from [1, 2, 3, 4]
+        self.layers_count = len(self.layers_returned)
+
+        self.out_dims = [384 ]
+        self.out_downsample_scales = []
+
+        if self.freeze:
+            for param in self.parameters():
+                param.requires_grad = False
+
     def forward(self, x):
-        dinov2_out = self.net.forward_features(resize(x, H_out=518, W_out=518)) # prev. 518, 518
-        patch_tokens = dinov2_out['x_prenorm'][:, 1:]
-        #patch_tokens = dinov2_out['x_norm_patchtokens']
+        x = resize(x, H_out=64 * 14, W_out=64 * 14)
+        x = self.extractor.forward_features(x)["x_norm_patchtokens"]  # # 'x_norm_patchtokens', 'x_prenorm'
+        x = x.reshape(-1, 64, 64, 384).permute(0, 3, 1, 2)
 
-        feat = resize(patch_tokens.reshape(-1, 37, 37, 384).permute(0, 3, 1, 2), H_out=64, W_out=64, mode='bilinear')
+        x_layers = [x]
+        return x_layers
 
-        return torch.nn.functional.normalize(self.doubleconv(feat), p=2, dim=1)
+
