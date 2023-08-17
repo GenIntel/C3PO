cc648fe96c95f8954bd25b59e41f7e592fb32efa
limpbot <leonhardsommer96@gmail.com>
2023-08-17 17:11:41 +0200

    fix visualization with uneven image size

--- src/od3d/methods/nemo/method.py
+++ src/od3d/methods/nemo/method.py
@@ -695,7 +695,7 @@ class NeMo(OD3D_Method):
 
                             img = get_img_from_plot(ax=ax, fig=fig)
                             plt.close(fig)
-                            img = resize(img, H_out=imgs.shape[-2], W_out=imgs.shape[-2])
+                            img = resize(img, H_out=imgs.shape[-2], W_out=imgs.shape[-1])
                             img = draw_text_in_rgb(img, fontScale=0.4, lineThickness=2, fontColor=(0, 0, 0), text=f'{batch_sel_scores[b]}\nmin={imgs_sim.min().item():.3f}\nmax={imgs_sim.max().item():.3f}')
                             imgs = torch.cat([imgs, img[None,].to(device=imgs.device)], dim=0)
                             #resize(img, )
