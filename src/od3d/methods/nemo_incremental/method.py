2c65e5cfac8c5332b0430a8ed8f0b33d65add293
limpbot <leonhardsommer96@gmail.com>
2023-09-12 11:43:47 +0200

    fix bug config.train.incremental.pseudo_labels_update.count_new_labels_proposed >= available

--- src/od3d/methods/nemo_incremental/method.py
+++ src/od3d/methods/nemo_incremental/method.py
@@ -64,7 +64,7 @@ class NeMo_Incremental(NeMo):
         train_dict_category_sequences_pseudo_labeled: Dict[str, List[str]] = {}
         train_dict_category_sequences_unlabeled_unrolled = OD3D_Meta.unroll_nested_metas(self.train_dict_category_sequences_unlabeled)
         if self.config.train.incremental.pseudo_labels_update.count_new_labels_proposed >= len(train_dict_category_sequences_unlabeled_unrolled):
-            train_dict_category_sequences_pseudo_labeled_proposed = train_dict_category_sequences_unlabeled_unrolled
+            train_dict_category_sequences_pseudo_labeled_proposed = OD3D_Meta.rollup_flattened_frames(train_dict_category_sequences_unlabeled_unrolled)
         else:
             train_dict_category_sequences_pseudo_labeled_proposed = OD3D_Meta.rollup_flattened_frames(random.sample(train_dict_category_sequences_unlabeled_unrolled, k=self.config.train.incremental.pseudo_labels_update.count_new_labels_proposed))
 
