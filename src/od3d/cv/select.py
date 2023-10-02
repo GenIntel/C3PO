99109fca3f8be5efbdfb90ea82e7e2ebe8718d01
limpbot <leonhardsommer96@gmail.com>
2023-10-02 14:53:30 +0200

    fix batch_indexMD_select

--- src/od3d/cv/select.py
+++ src/od3d/cv/select.py
@@ -39,5 +39,55 @@ def batched_index_select(input, index, dim=None):
 
     return torch.gather(input, dim, index)
 
+def index_MD_to_1D(indexMD, inputMD, dims):
+    """
+    Args:
+        input: B...x...xI1x...xI2...
+        dims (List[int]): dimensions of input which should be indexed [I1, ..., IM]
+        indexMD: B...xNxM
+    Returns:
+        index1D: B...xN
+    """
 
+    M = len(dims)
+    index1D = torch.zeros_like(indexMD[..., 0])
+    for m in range(M):
+        if m > 0:
+            index1D *= inputMD.shape[dims[m]]
+        index1D += indexMD[..., m]
+    return index1D
 
+def batched_indexMD_select(inputMD, indexMD, dims=None):
+    """
+    Args:
+        input: B...x...xI1x...xI2...
+        dims (List[int]): dimensions of input which should be indexed [I1, ..., IM]
+        indexMD: B...xNxM
+    Returns:
+        out: B...xNx...
+    """
+
+    batch_dims = indexMD.shape[:-2]
+    batch_dims_count = len(batch_dims)
+
+    if dims is None:
+        dims = batch_dims_count + torch.arange(indexMD.shape[-1])
+
+    assert(len(dims) == indexMD.shape[-1])
+
+    M = len(dims)
+    non_batch_non_index_dims_count = inputMD.dim() - batch_dims_count - M
+
+    index1D = index_MD_to_1D(indexMD, inputMD, dims)
+
+    input_permutation_MD_to_1D = torch.LongTensor([i for i in range(batch_dims_count)])
+    input_permutation_MD_to_1D = torch.cat([input_permutation_MD_to_1D, torch.LongTensor([d for d in dims])])
+    input_permutation_MD_to_1D = torch.cat([input_permutation_MD_to_1D, torch.LongTensor([i for i in set(list(range(inputMD.dim())))-set(input_permutation_MD_to_1D.tolist())])])
+    input1D_shape = batch_dims + (-1,)
+    if non_batch_non_index_dims_count > 0:
+        input1D_shape += torch.Size([inputMD.shape[i] for i in input_permutation_MD_to_1D[-non_batch_non_index_dims_count:]])
+    input1D = inputMD.permute(*input_permutation_MD_to_1D).view(input1D_shape)
+
+    out1D = batched_index_select(input=input1D, index=index1D)
+
+    return out1D
