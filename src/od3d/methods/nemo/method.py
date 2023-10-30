2d47a04bb84283cf278e5154b53cfc85f0c718ff
limpbot <leonhardsommer96@gmail.com>
2023-10-30 21:01:30 +0100

    fix bug multiview scale

--- src/od3d/methods/nemo/method.py
+++ src/od3d/methods/nemo/method.py
@@ -1242,10 +1242,11 @@ class NeMo(OD3D_Method):
                     mask_pxl2d = get_pxl2d(H=feats2d_net_mask.shape[-2], W=feats2d_net_mask.shape[-1], dtype=float, device=feats2d_net_mask.device)
                     mask_pxl2d = mask_pxl2d[None, None].expand(*feats2d_net_mask.shape, 2) * self.down_sample_rate
                     for b in range(B):
-                        mask_bbox_x_max[b] = mask_pxl2d[b][mask[b]][:, 0].max() - cx[b]
-                        mask_bbox_x_min[b] = mask_pxl2d[b][mask[b]][:, 0].min() - cx[b]
-                        mask_bbox_y_max[b] = mask_pxl2d[b][mask[b]][:, 1].max() - cy[b]
-                        mask_bbox_y_min[b] = mask_pxl2d[b][mask[b]][:, 1].min() - cy[b]
+                        if mask[b].sum() > 0:
+                            mask_bbox_x_max[b] = mask_pxl2d[b][mask[b]][:, 0].max() - cx[b]
+                            mask_bbox_x_min[b] = mask_pxl2d[b][mask[b]][:, 0].min() - cx[b]
+                            mask_bbox_y_max[b] = mask_pxl2d[b][mask[b]][:, 1].max() - cy[b]
+                            mask_bbox_y_min[b] = mask_pxl2d[b][mask[b]][:, 1].min() - cy[b]
                 # B x 4
                 scales = torch.stack([mask_bbox_x_max / mesh_bbox_x_max, mask_bbox_x_min / mesh_bbox_x_min, mask_bbox_y_max / mesh_bbox_y_max, mask_bbox_y_min / mesh_bbox_y_min], dim=-1)
                     # torch.masked_select(input=mask_pxl2d, mask=feats2d_net_mask > 0.5)
