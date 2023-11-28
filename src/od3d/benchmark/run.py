8dde9c8349c79037fb2f672727d2f683516a3799
limpbot <leonhardsommer96@gmail.com>
2023-11-28 18:18:24 +0100

    fix torque run

--- src/od3d/benchmark/run.py
+++ src/od3d/benchmark/run.py
@@ -1,3 +1,5 @@
+import logging
+logger = logging.getLogger(__name__)
 import subprocess
 from omegaconf import DictConfig, OmegaConf
 from od3d.benchmark.benchmark import OD3D_Benchmark
@@ -44,10 +46,36 @@ def bench_single_method_torque(cfg: DictConfig):
     with open(local_tmp_script_fpath, 'w') as rsh:
 
         gpu_count = cfg.platform.gpu_count
+        gpu_mem_in_gb = cfg.platform.gpu_mem_in_gb
         node_count = 1
         cpu_count = cfg.platform.cpu_count
         ram = cfg.platform.ram
         walltime = cfg.platform.walltime
+
+        if gpu_count > 0:
+            if gpu_mem_in_gb > 16:
+                if gpu_mem_in_gb > 24:
+                    logger.warning(f'GPU memory of {gpu_mem_in_gb} GB is too large. Using 24 GB instead.')
+                #gpu_mem_cfg_str = ':nvidiaMin24GB'
+                gpu_mem_cfg_str = ':nvidiaRTX3090'
+            elif gpu_mem_in_gb > 12:
+                # gpu_mem_cfg_str = ':nvidiaMin16GB'
+                gpu_mem_cfg_str = ':nvidiaP100'
+            elif gpu_mem_in_gb > 11:
+                # gpu_mem_cfg_str = ':nvidiaMin12GB'
+                gpu_mem_cfg_str = ':nvidiaP100'
+            elif gpu_mem_in_gb > 10:
+                # gpu_mem_cfg_str = ':nvidiaMin11GB'
+                gpu_mem_cfg_str = ':nvidiaRTX2080Ti'
+            elif gpu_mem_in_gb > 6:
+                # gpu_mem_cfg_str = ':nvidiaMin8GB'
+                gpu_mem_cfg_str = ':nvidiaRTX2080Ti'
+            elif gpu_mem_in_gb > 0:
+                gpu_mem_cfg_str = ':nvidiaRTX2080Ti'
+            else:
+                gpu_mem_cfg_str = ':nvidiaRTX2080Ti'
+        else:
+            gpu_mem_cfg_str = ""
 
         gpu_cfg_str = f':gpus={gpu_count}' if gpu_count > 0 else ""
         cuda_cfg_str = f':nvidiaMinCC75' if gpu_count > 0 else ""
@@ -83,7 +111,7 @@ pip install -e {cfg.platform.path_od3d}
         script_as_string = f'''#!/bin/bash
 #PBS -N {job_name}
 #PBS -S /bin/bash
-#PBS -l nodes={node_count}:ppn={cpu_count}{gpu_cfg_str}{cuda_cfg_str},mem={ram},walltime={walltime}
+#PBS -l nodes={node_count}:ppn={cpu_count}{gpu_cfg_str}{gpu_mem_cfg_str}{cuda_cfg_str},mem={ram},walltime={walltime}
 #PBS -q default-cpu
 #PBS -m a
 #PBS -M {cfg.platform.username}@informatik.uni-freiburg.de
@@ -174,6 +202,7 @@ def bench_single_method_slurm(cfg: DictConfig):
 
     with open(local_tmp_script_fpath, 'w') as rsh:
         gpu_count = cfg.platform.gpu_count
+        gpu_mem_in_gb = cfg.platform.gpu_mem_in_gb
         node_count = 1
         cpu_count = cfg.platform.cpu_count
         ram = cfg.platform.ram
