7cbfa1e5bc32031568dc0e62e83f253c7be6f435
limpbot <leonhardsommer96@gmail.com>
2024-03-11 19:59:48 +0100

    fix dist calc for 11 gG

--- src/od3d/datasets/sequence.py
+++ src/od3d/datasets/sequence.py
@@ -1224,7 +1224,7 @@ class OD3D_SequenceMeshMixin(OD3D_MeshFeatsTypeMixin, OD3D_MeshTypeMixin, OD3D_S
             else:
                 logger.warning(f'unknown embed type {embed_type}')
 
-            P = 64  # ensures that 11 GB are enough
+            P = 80  # ensures that 11 GB are enough
             logger.info(f'seq1 verts {seq1_verts_count}, seq2 verts {seq2_verts_count}, seq1 partial {(seq1_verts_count // P)}, viewpoints max {V}')
 
             for p in range(P):
