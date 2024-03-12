0845196582407fced88404f60087f5fbdcd9d31b
limpbot <leonhardsommer96@gmail.com>
2024-03-12 08:54:35 +0100

    fix ensure alphawrap shape works

--- src/od3d/datasets/sequence.py
+++ src/od3d/datasets/sequence.py
@@ -724,7 +724,7 @@ class OD3D_SequenceMeshMixin(OD3D_MeshFeatsTypeMixin, OD3D_MeshTypeMixin, OD3D_S
             alpha = particle_size / 2.
             # while vertices_count > mesh_vertices_count:
             #     alpha = alpha * 1.3
-            
+
             o3d_obj_mesh = None
             while o3d_obj_mesh is None:
                 try:
@@ -747,53 +747,72 @@ class OD3D_SequenceMeshMixin(OD3D_MeshFeatsTypeMixin, OD3D_MeshTypeMixin, OD3D_S
             obj_mesh = Mesh.from_o3d(o3d_obj_mesh_downsampled, device=device)
 
         elif mesh_type == 'alphawrap':
-            # #### OPTION 3: ALPHA_SHAPE
-            pts3d = random_sampling(pts3d, pts3d_max_count=10000) # 11 GB
+            # #### OPTION 3: ALPHAWRAP_SHAPE
+            pts3d = random_sampling(pts3d, pts3d_max_count=10000)  # 11 GB
             quantile = max(0.01, 3. / len(pts3d))
             particle_size = torch.cdist(pts3d[None,], pts3d[None,]).quantile(dim=-1, q=quantile).mean()
-            alpha = particle_size / 2.
-            offset = particle_size / 20.
             vertices_count = mesh_vertices_count + 1
+            alpha = particle_size / 2.
+
+            o3d_obj_mesh = None
             while vertices_count > mesh_vertices_count:
+                try:
+                    o3d_obj_mesh = open3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(o3d_pcl, alpha)
+                    vertices_count = len(o3d_obj_mesh.vertices)
+                    logger.info(o3d_obj_mesh)
+                except Exception as e:
+                    logger.warning(f'alpha {alpha} failed with {e}')
                 alpha = alpha * 1.3
-                from CGAL.CGAL_Kernel import Point_3
-                from CGAL.CGAL_Alpha_wrap_3 import alpha_wrap_3
-                from CGAL.CGAL_Polyhedron_3 import Polyhedron_3
-                cgal_pts3d = [ Point_3(pt[0].item(), pt[1].item(), pt[2].item()) for pt in pts3d ]
-                cgal_poly = Polyhedron_3()
-                alpha_wrap_3(cgal_pts3d, alpha.item(), offset.item(), cgal_poly)
-                cgal_poly.points()
 
-                vertices = []
-                for v in cgal_poly.vertices():
-                    vertices.append([v.point().x(), v.point().y(), v.point().z()])
-                vertices = torch.Tensor(vertices)
+            o3d_obj_mesh = o3d_obj_mesh.remove_unreferenced_vertices()
+            logger.info(o3d_obj_mesh)
 
-                # Get faces
-                faces = []
-                for f in cgal_poly.facets():
-                    edge = f.facet_begin()
-                    edge = edge.next()
-                    face_vertices = []
-                    for i in range(f.facet_degree()):
-                        vertex = torch.Tensor([edge.vertex().point().x(), edge.vertex().point().y(), edge.vertex().point().z()])
-                        vertex_id = torch.where((vertices == vertex).all(dim=-1))[0]
-                        face_vertices.append(vertex_id)
-                        edge = edge.next()
-                    # Assuming each facet is a triangle
-                    assert len(face_vertices) == 3
-                    faces.append(face_vertices)
-                faces = torch.Tensor(faces).long()
-
-                vertices = open3d.utility.Vector3dVector(vertices.detach().cpu().numpy())
-                faces = open3d.utility.Vector3iVector(faces.detach().cpu().numpy())
-
-                o3d_obj_mesh = open3d.geometry.TriangleMesh(vertices=vertices, triangles=faces)
-
-                assert o3d_obj_mesh.is_watertight()
-
-                logger.info(o3d_obj_mesh)
-                vertices_count = len(o3d_obj_mesh.vertices)
+            # pts3d = random_sampling(pts3d, pts3d_max_count=10000) # 11 GB
+            # quantile = max(0.01, 3. / len(pts3d))
+            # particle_size = torch.cdist(pts3d[None,], pts3d[None,]).quantile(dim=-1, q=quantile).mean()
+            # alpha = particle_size / 2.
+            # offset = particle_size / 20.
+            # vertices_count = mesh_vertices_count + 1
+            # while vertices_count > mesh_vertices_count:
+            #     alpha = alpha * 1.3
+            #     from CGAL.CGAL_Kernel import Point_3
+            #     from CGAL.CGAL_Alpha_wrap_3 import alpha_wrap_3
+            #     from CGAL.CGAL_Polyhedron_3 import Polyhedron_3
+            #     cgal_pts3d = [ Point_3(pt[0].item(), pt[1].item(), pt[2].item()) for pt in pts3d ]
+            #     cgal_poly = Polyhedron_3()
+            #     alpha_wrap_3(cgal_pts3d, alpha.item(), offset.item(), cgal_poly)
+            #     cgal_poly.points()
+            #
+            #     vertices = []
+            #     for v in cgal_poly.vertices():
+            #         vertices.append([v.point().x(), v.point().y(), v.point().z()])
+            #     vertices = torch.Tensor(vertices)
+            #
+            #     # Get faces
+            #     faces = []
+            #     for f in cgal_poly.facets():
+            #         edge = f.facet_begin()
+            #         edge = edge.next()
+            #         face_vertices = []
+            #         for i in range(f.facet_degree()):
+            #             vertex = torch.Tensor([edge.vertex().point().x(), edge.vertex().point().y(), edge.vertex().point().z()])
+            #             vertex_id = torch.where((vertices == vertex).all(dim=-1))[0]
+            #             face_vertices.append(vertex_id)
+            #             edge = edge.next()
+            #         # Assuming each facet is a triangle
+            #         assert len(face_vertices) == 3
+            #         faces.append(face_vertices)
+            #     faces = torch.Tensor(faces).long()
+            #
+            #     vertices = open3d.utility.Vector3dVector(vertices.detach().cpu().numpy())
+            #     faces = open3d.utility.Vector3iVector(faces.detach().cpu().numpy())
+            #
+            #     o3d_obj_mesh = open3d.geometry.TriangleMesh(vertices=vertices, triangles=faces)
+            #
+            #     assert o3d_obj_mesh.is_watertight()
+            #
+            #     logger.info(o3d_obj_mesh)
+            #     vertices_count = len(o3d_obj_mesh.vertices)
 
             # tol = particle_size / 2.
             # import pymesh2
