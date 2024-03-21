d964057080307c0d137c668477e57fac37f7dadc
limpbot <leonhardsommer96@gmail.com>
2024-03-21 16:37:36 +0100

    add relative path option

--- src/od3d/cli/dataset.py
+++ src/od3d/cli/dataset.py
@@ -156,13 +156,14 @@ def rsync_raw(dataset: str = typer.Option('co3d_only_first', '-d', '--dataset'),
 @app.command()
 def rsync_preprocess(dataset: str = typer.Option('co3d_only_first', '-d', '--dataset'),
           platform_source: str = typer.Option('local', '-s', '--source'),
-          platform_target: str = typer.Option('slurm', '-t', '--target')):
+          platform_target: str = typer.Option('slurm', '-t', '--target'),
+          rpath: str = typer.Option('', '-r', '--relative-path')):
     logging.basicConfig(level=logging.INFO)
     config_source = od3d.io.load_hierarchical_config(platform=platform_source, overrides=["+datasets@dataset=" + dataset])
     config_target = od3d.io.load_hierarchical_config(platform=platform_target, overrides=["+datasets@dataset=" + dataset])
 
-    paths_source = Path(config_source.dataset.path_preprocess)
-    paths_target = Path(config_target.dataset.path_preprocess)
+    paths_source = Path(config_source.dataset.path_preprocess).joinpath(rpath)
+    paths_target = Path(config_target.dataset.path_preprocess).joinpath(rpath)
 
     source_link = f'{config_source.platform.link}:' if config_source.platform.link != 'local' else ''
     target_link = f'{config_target.platform.link}:' if config_target.platform.link != 'local' else ''
