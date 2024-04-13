234ab26911c95b9e7e3627dfeef1f78f2d8ffb4e
Premnath Srinivasan <sriprem1130@gmail.com>
2024-04-13 20:06:15 +0200

    Refactor Pascal3D_OccFrame class

--- src/od3d/datasets/pascal3d_occ/dataset.py
+++ src/od3d/datasets/pascal3d_occ/dataset.py
@@ -4,18 +4,20 @@ from od3d.datasets.dataset import OD3D_Dataset
 from omegaconf import DictConfig
 from pathlib import Path
 from od3d.datasets.pascal3d import Pascal3D
-
+from od3d.datasets.object import OD3D_FRAME_DEPTH_TYPES
+from od3d.datasets.frame import OD3D_FRAME_KPTS2D_ANNOT_TYPES
 from od3d.datasets.pascal3d.enum import PASCAL3D_CATEGORIES, MAP_CATEGORIES_OD3D_TO_PASCAL3D
 from od3d.datasets.frame import OD3D_FRAME_MODALITIES
 from typing import Dict, List
 import shutil
 import od3d.io
-
+from od3d.datasets.pascal3d.frame import Pascal3DFrameMeta , Pascal3DFrame
+from od3d.datasets.object import OD3D_MESH_TYPES
 from od3d.datasets.pascal3d.frame import Pascal3DFrameMeta
 from od3d.datasets.pascal3d_occ.frame import Pascal3D_OccFrameMeta, Pascal3D_OccFrame
-
+from od3d.cv.geometry.mesh import Meshes
 from tqdm import tqdm
-
+from od3d.datasets.pascal3d.enum import PASCAL3D_SCALE_NORMALIZE_TO_REAL, PASCAL3D_CATEGORIES 
 from od3d.data.ext_enum import ExtEnum
 
 class PASCAL3D_OCC_SUBSETS(str, ExtEnum):
@@ -25,8 +27,9 @@ class PASCAL3D_OCC_SUBSETS(str, ExtEnum):
 
 
 class Pascal3D_Occ(OD3D_Dataset):
-    CATEGORIES = PASCAL3D_CATEGORIES
-    MAP_OD3D_CATEGORIES = MAP_CATEGORIES_OD3D_TO_PASCAL3D
+    all_categories = list(PASCAL3D_CATEGORIES)
+    map_od3d_categories = MAP_CATEGORIES_OD3D_TO_PASCAL3D
+    frame_type = Pascal3D_OccFrame
 
     def __init__(
             self,
@@ -43,9 +46,13 @@ class Pascal3D_Occ(OD3D_Dataset):
             index_shift=0,
     ):
         if categories is not None:
-            categories = [self.MAP_OD3D_CATEGORIES[category] if category not in self.CATEGORIES.list() else category for category in categories]
+            if self.map_od3d_categories is not None:
+                self.categories = [self.map_od3d_categories.get(category, category) if category not in self.all_categories else category for category in categories]
+                print(f'categories: {self.categories}')
+            else:
+                self.categories = categories
         else:
-            categories = self.CATEGORIES.list()
+            self.categories = self.all_categories
         super().__init__(categories=categories, name=name,
                          modalities=modalities, path_raw=path_raw,
                          path_preprocess=path_preprocess, transform=transform,
@@ -61,12 +68,12 @@ class Pascal3D_Occ(OD3D_Dataset):
                             dict_nested_frames=dict_nested_frames, transform=self.transform,
                             index_shift=self.index_shift, path_pascal3d_raw=self.path_pascal3d_raw)
 
-    def get_item(self, item):
-        frame_meta = Pascal3D_OccFrameMeta.load_from_meta_with_name_unique(path_meta=self.path_meta,
-                                                                       name_unique=self.list_frames_unique[item])
-        return Pascal3D_OccFrame(path_raw=self.path_raw, path_preprocess=self.path_preprocess, path_meta=self.path_meta,
-                                 path_meshes=self.path_meshes, meta=frame_meta, modalities=self.modalities,
-                                 categories=self.categories)
+    # def get_item(self, item):
+    #     frame_meta = Pascal3D_OccFrameMeta.load_from_meta_with_name_unique(path_meta=self.path_meta,
+    #                                                                    name_unique=self.list_frames_unique[item])
+    #     return Pascal3D_OccFrame(path_raw=self.path_raw, path_preprocess=self.path_preprocess, path_meta=self.path_meta,
+    #                              path_meshes=self.path_meshes, meta=frame_meta, modalities=self.modalities,
+    #                              categories=self.categories)
     @staticmethod
     def setup(config: DictConfig):
         path_raw = Path(config.path_raw)
@@ -86,7 +93,66 @@ class Pascal3D_Occ(OD3D_Dataset):
             od3d.io.download(url=config.url_pascal3d_occ_script, fpath=fpath)
             od3d.io.run_cmd(cmd=f'chmod +x {fpath}', logger=logger, live=True)
             od3d.io.run_cmd(cmd=f'cd {path_raw} && {fpath}', logger=logger, live=True)
+    def preprocess(self, config_preprocess: DictConfig):
+        logger.info("preprocess")
+        for key in config_preprocess.keys():
+            if key == 'cuboid' and config_preprocess.cuboid.get('enabled', False):
+                override = config_preprocess.cuboid.get('override', False)
+                remove_previous = config_preprocess.cuboid.get('remove_previous', False)
+                self.preprocess_cuboid(override=override, remove_previous=remove_previous)
+            elif key == 'mask' and config_preprocess.mask.get('enabled', False):
+                override = config_preprocess.mask.get('override', False)
+                remove_previous = config_preprocess.mask.get('remove_previous', False)
+                self.preprocess_mask(override=override, remove_previous=remove_previous)
+            elif key == 'depth' and config_preprocess.depth.get('enabled', False):
+                override = config_preprocess.depth.get('override', False)
+                remove_previous = config_preprocess.depth.get('remove_previous', False)
+                self.preprocess_depth(override=override, remove_previous=remove_previous)
 
+    def preprocess_cuboid(self, override=False, remove_previous=False):
+        logger.info('preprocess cuboid...')
+
+        for category in self.categories:
+            if category not in self.all_categories:
+                continue
+            mesh_types = [OD3D_MESH_TYPES.CUBOID250, OD3D_MESH_TYPES.CUBOID500, OD3D_MESH_TYPES.CUBOID1000]
+            for mesh_type in mesh_types:
+                fpath_mesh_out = self.path_preprocess.joinpath(Pascal3D_OccFrame.get_rfpath_pp_categorical_mesh(mesh_type=mesh_type, category=category))
+
+                if fpath_mesh_out.exists() and not override:
+                    logger.warning(f'mesh already exists {fpath_mesh_out}')
+                    return
+                else:
+                    logger.info(f'preprocessing mesh for {category} with type {mesh_type}')
+
+                import re
+                match = re.match(r"([a-z]+)([0-9]+)", mesh_type, re.I)
+                if match and len(match.groups()) == 2:
+                    mesh_type, mesh_vertices_count = match.groups()
+                    mesh_vertices_count = int(mesh_vertices_count)
+                else:
+                    msg = f'could not retrieve mesh type and vertices count from mesh name {mesh_type}'
+                    raise Exception(msg)
+
+                fpaths_meshes_category = [fpath for fpath in self.path_pascal3d_raw.joinpath(Pascal3DFrame.get_rpath_raw_categorical_meshes(category=category)).iterdir()]
+                meshes = Meshes.load_from_files(fpaths_meshes_category)
+                meshes.verts.data = meshes.verts * PASCAL3D_SCALE_NORMALIZE_TO_REAL[category]
+                pts3d = meshes.verts
+
+                from od3d.cv.geometry.fit.cuboid import fit_cuboid_to_pts3d
+
+
+                cuboids, _ = fit_cuboid_to_pts3d(pts3d=pts3d,
+                                                 optimize_rot=False,
+                                                 optimize_transl=False,
+                                                 vertices_max_count=mesh_vertices_count,
+                                                 optimize_steps=1)
+
+                # show:
+                #Meshes.load_from_meshes([meshes.get_mesh_with_id(i) for i in range(meshes.meshes_count)] + [cuboids.get_mesh_with_id(0)]).show(meshes_add_translation=False)
+
+                obj_mesh = cuboids.get_mesh_with_id(0)
+                obj_mesh.write_to_file(fpath=fpath_mesh_out)
     #### PREPROCESS META
     @staticmethod
     def extract_meta(config: DictConfig):
@@ -170,16 +236,30 @@ class Pascal3D_Occ(OD3D_Dataset):
                                                    l_kpts3d=pascal3d_frame_meta.l_kpts3d,
                                                    l_kpts2d_annot=pascal3d_frame_meta.l_kpts2d_annot,
                                                    l_kpts2d_annot_vsbl=pascal3d_frame_meta.l_kpts2d_annot_vsbl,
-                                                   kpts_names=pascal3d_frame_meta.kpts_names, rfpath_mask=rfpath_mask,
+                                                   kpts_names=pascal3d_frame_meta.kpts_names,
                                                    l_bbox=l_bbox)
 
 
                 if frame_meta is not None:
                     frame_meta.save(path_meta=path_meta)
 
-    def preprocess(self, override: bool = False):
-        pass
-
+    ##### DATASET PROPERTIES
+    def get_frame_by_name_unique(self, name_unique):
+        from od3d.datasets.object import OD3D_CAM_TFORM_OBJ_TYPES, OD3D_FRAME_MASK_TYPES, OD3D_MESH_TYPES, \
+            OD3D_MESH_FEATS_TYPES, OD3D_MESH_FEATS_DIST_REDUCE_TYPES, \
+            OD3D_TFROM_OBJ_TYPES
+        return self.frame_type(path_raw=self.path_raw, path_preprocess=self.path_preprocess,
+                               path_meshes=self.path_meshes,
+                               name_unique=name_unique, all_categories=self.categories,
+                               mask_type=OD3D_FRAME_MASK_TYPES.MESH,
+                               cam_tform4x4_obj_type=OD3D_CAM_TFORM_OBJ_TYPES.META,
+                               mesh_type=OD3D_MESH_TYPES.META,
+                               mesh_feats_type=OD3D_MESH_FEATS_TYPES.M_DINOV2_VITB14_FROZEN_BASE_NO_NORM_T_CENTERZOOM512_R_ACC,
+                               mesh_feats_dist_reduce_type=OD3D_MESH_FEATS_DIST_REDUCE_TYPES.MIN_AVG,
+                               modalities=self.modalities,
+                               tform_obj_type=OD3D_TFROM_OBJ_TYPES.RAW,
+                               depth_type=OD3D_FRAME_DEPTH_TYPES.MESH,
+                               kpts2d_annot_type=OD3D_FRAME_KPTS2D_ANNOT_TYPES.META,)
     @property
     def path_meshes(self):
-        return Pascal3D.get_path_meshes(path_raw=self.path_pascal3d_raw)
+        return self.path_pascal3d_raw
